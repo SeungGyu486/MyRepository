b9301ffb05a2d9edb441555a31e7808cdf571747
Lothric <Lothric@DESKTOP-NLVNHTP>
2022-06-13 10:53:21 +0900

    Change in master after branching dev

--- HelloGit/src/GIthub/MyClass.java
+++ HelloGit/src/GIthub/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!");
 		System.out.println("This is ????Version.");
+		System.out.println("Change in master bracn.");
 	}
 
 }
--- HelloGit/src/GIthub/MyHello.java
+++ HelloGit/src/GIthub/MyHello.java
@@ -3,5 +3,6 @@ package GIthub;
 public class MyHello {
 	public void sayHello() {
 		System.out.println("Hello!");
+		System.out.println("sayHello change in master branch.");
 	}
 }
