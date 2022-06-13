68536454cc0df4b0695bb9cd7249c8634a3daa4d
Lothric <Lothric@DESKTOP-NLVNHTP>
2022-06-13 11:11:17 +0900

    HW MyGit

---
+++ MyGit/src/HW/B.java
@@ -0,0 +1,10 @@
+package HW;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("Hello. I'm B java.");
+
+	}
+
+}
---
+++ MyGit/src/HW/C.java
@@ -0,0 +1,10 @@
+package HW;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("Hello. I'm C java.");
+
+	}
+
+}
