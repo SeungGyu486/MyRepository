b3226cd54f95857f036c80a6b8268bd52cdca419
Lothric <Lothric@DESKTOP-NLVNHTP>
2022-06-13 10:49:14 +0900

    First commit in dev branch

--- HelloGit/src/GIthub/MyClass.java
+++ HelloGit/src/GIthub/MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!");
 		System.out.println("This is ????Version.");
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 	}
 
 }
